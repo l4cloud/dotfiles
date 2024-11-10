e597f07a370f76f01c054ce811edf8fcd985cfbd
l4 <lewisculleton00@gmail.com>
2024-11-10 23:26:51 +0000

    Added deps

--- misc/deps_ubuntu.sh
+++ misc/deps_ubuntu.sh
@@ -2,10 +2,19 @@
 
 echo "installing dependencies for ubuntu"
 
-
-dependencies = ["stow", "xclip", "zsh"]
+dependencies="tmux stow xclip zsh ninja-build gettext cmake unzip curl build-essential"
 
 sudo apt update
 sudo apt install $dependencies
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+chsh -s $(which zsh)
 curl -sS https://starship.rs/install.sh | sh
+
+cd /tmp
+
+git clone https://github.com/neovim/neovim.git
+cd /tmp/neovim && git checkout stable
+
+make CMAKE_BUILD_TYPE=Release
+sudo make install
