15cbeb14e641c982c6cd5702c9f70bdf59255e50
l4 <lewisculleton00@gmail.com>
2024-11-26 17:31:48 +0000

    Updated rose pine

--- .config/nvim/lua/plugins/rosepine.lua
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -2,5 +2,15 @@ return {
   {
     'rose-pine/neovim',
     name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        variant = 'auto', -- auto, main, moon, or dawn
+        dark_variant = 'main', -- main, moon, or dawn
+        dim_inactive_windows = false,
+        extend_background_behind_borders = true,
+      }
+
+      vim.cmd 'colorscheme rose-pine'
+    end,
   },
 }
