f709e7282651e156450536e372e611e7fc78b868
lewis-culleton <lewis.culleton@lexisnexisrisk.com>
2025-02-20 14:39:28 +0000

    updated lualine, cmdheight and relative line numbers

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -20,6 +20,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+vim.opt.cmdheight = 0
+vim.opt.relativenumber = true
 
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local theme = require 'lualine.themes.gruvbox_light'
+    local theme = require 'lualine.themes.rose-pine'
     theme.normal.c.bg = nil
 
     require('lualine').setup {
