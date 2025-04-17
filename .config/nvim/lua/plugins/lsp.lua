a8fb10aae560da9c785537fe0c7b3d848731ff35
lewis-culleton <lewis.culleton@lexisnexisrisk.com>
2025-04-17 11:46:29 +0100

    added bash ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -154,6 +154,7 @@ return {
         eslint = {},
         html = {},
         emmet_ls = {},
+        bashls = {},
       }
       require('mason').setup()
       local ensure_installed = vim.tbl_keys(servers or {})
@@ -167,7 +168,7 @@ return {
           function(server_name)
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
+            -- by the servor configuration above. Useful when disabling
             -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
