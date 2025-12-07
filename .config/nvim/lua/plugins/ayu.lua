ea3f0e38508056f9a147b08eaa8ed4d4647e6ee8
Lu <lculleton@protonmail.com>
2025-12-07 01:27:39 +0000

    updated config

--- .config/nvim/lua/plugins/ayu.lua
+++ .config/nvim/lua/plugins/ayu.lua
@@ -5,17 +5,19 @@ return {
     require('ayu').setup {
       mirage = false, -- Set to false to use dark variant instead of mirage
       terminal = true, -- Let the theme manage terminal colors
-      overrides = {
-        Normal = { bg = 'None' },
-        NormalFloat = { bg = 'None' },
-        ColorColumn = { bg = 'None' },
-        SignColumn = { bg = 'None' },
-        Folded = { bg = 'None' },
-        FoldColumn = { bg = 'None' },
-        CursorLine = { bg = 'None' },
-        CursorColumn = { bg = 'None' },
-        VertSplit = { bg = 'None' },
-      }, -- Customize specific highlight groups if needed
+       overrides = {
+         Normal = { bg = 'None' },
+         NormalFloat = { bg = 'None' },
+         ColorColumn = { bg = 'None' },
+         SignColumn = { bg = 'None' },
+         Folded = { bg = 'None' },
+         FoldColumn = { bg = 'None' },
+         CursorLine = { bg = 'None' },
+         CursorColumn = { bg = 'None' },
+         VertSplit = { bg = 'None' },
+         StatusLine = { bg = 'None' },
+         StatusLineNC = { bg = 'None' },
+       }, -- Customize specific highlight groups if needed
     }
     vim.cmd 'colorscheme ayu-dark' -- Apply the dark variant
   end,
