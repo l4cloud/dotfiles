7d6da690d169f00c0e889829c0f7ab7d7bc24be9
Lu <lculleton@protonmail.com>
2025-12-18 14:21:25 +0000

    Added wallapers to config for stow

--- .config/hypr/wallpaper.sh
+++ .config/hypr/wallpaper.sh
@@ -2,7 +2,7 @@
 WALLPAPER_DIR="$HOME/Wallpapers"
 #I dont know what the fuck I am doing
 menu() {
-    find "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
+    find -L "${WALLPAPER_DIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) | awk '{print "img:"$0}'
 }
 main() {
     choice=$(menu | wofi -c ~/.config/wofi/wallpaper -s ~/.config/wofi/style-wallpaper.css --show dmenu --prompt "Select Wallpaper:" -n)
